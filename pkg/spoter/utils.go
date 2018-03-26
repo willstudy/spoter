c02e341e856688c91d11edcb928219cdefd34c98
Liu Chang <studywiller@gmail.com>
2018-03-26 15:23:08 +0800

    Add utils functions

---
+++ pkg/spoter/utils.go
@@ -0,0 +1,17 @@
+package spoter
+
+func AllocMachine(model string) (string, error) {
+	return "", nil
+}
+
+func InstallDocker(target string) error {
+	return nil
+}
+
+func InstallK8sBase(target string) error {
+	return nil
+}
+
+func JoinIntoCluster(target string) error {
+	return nil
+}
