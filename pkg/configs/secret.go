fb75b131d73b9365f056b62006cf158b3c4b7201
cheng.he <genius.cirno@foxmail.com>
2018-05-13 18:59:20 +0800

    ?

--- pkg/configs/secret.go
+++ pkg/configs/secret.go
@@ -6,7 +6,7 @@ const (
 	Region          = "cn-hongkong"
 	ImageID         = "centos_7_04_64_20G_alibase_201701015.vhd"
 	InstanceType    = "ecs.xn4.small"
-	SecurityGroupID = "sg-bp1chtb8a3vdo2grma4f"
+	SecurityGroupID = "sg-j6cbhv38slef52pmafgn"
 	SpotPriceLimit  = "0.08"
 	SSHKeyName      = "hk-spoter-test"
 )
