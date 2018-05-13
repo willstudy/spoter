04edd3c5385965b7b8663b13cfcad87bf2c000ee
cheng.he <genius.cirno@foxmail.com>
2018-05-13 17:48:45 +0800

    config

--- pkg/configs/config.go
+++ pkg/configs/config.go
@@ -3,8 +3,8 @@ package configs
 const (
 	KubeadmCMD       = "/home/spoter/k8s-base/kubeadm"
 	RemoteKubeadmCMD = "/usr/bin/kubeadm"
-	KubeMaster       = "172.18.49.126:6443"
-	DiscoveryToken   = "sha256:319d4c3d9dbe98002931143c9cc23c7544a705f0e2c7702b57e0b033b62853a8"
+	KubeMaster       = "172.31.250.28:6443"
+	DiscoveryToken   = "sha256:b44288c64f48cff8280f0109b7329efff23d2c054475313216140633571283c2"
 	//JoinCMD        = "kubeadm join --token ba3a9c.a8a982e69445c017 139.59.169.189:6443 --discovery-token-ca-cert-hash sha256:203738e32ce7a9b85848fd49ae6dbb375949716a61413be605d7029cc4e4b700"
 
 	KubectlCMD = "/home/spoter/k8s-base/kubectl"
