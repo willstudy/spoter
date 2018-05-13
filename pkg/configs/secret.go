27ce8724b8ce386d478be861a95dfaf1be603d64
cheng.he <genius.cirno@foxmail.com>
2018-05-13 18:03:12 +0800

    fucking

--- pkg/configs/secret.go
+++ pkg/configs/secret.go
@@ -1,12 +1,12 @@
 package configs
 
 const (
-	AccessKey       = "LTAIHWDHBjiP5ji7"
-	SecretKey       = "MfgzMDhfX6sLEfQPMoTlkvXf5SZ2V0"
-	Region          = "cn-beijing"
+	AccessKey       = "LTAIf7pq1fsO3rfq"
+	SecretKey       = "FZoyHEoSjJY6HYiLpdygUiBfP5PMwe"
+	Region          = "cn-hongkong"
 	ImageID         = "centos_7_04_64_20G_alibase_201701015.vhd"
 	InstanceType    = "ecs.xn4.small"
-	SecurityGroupID = "sg-2ze65h1sejzb0yuf427v"
+	SecurityGroupID = "sg-j6cbhv38slef52pmafgn"
 	SpotPriceLimit  = "0.08"
 	SSHKeyName      = "k8s"
 )
